09272de1a5e483527713bc2052dc1aee3a354c43
FREEDOM <freewhynane62@gmail.com>
2022-01-26 09:03:11 +0000

    add section component

---
+++ src/Components/Sections.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Sections() {
+  return <Wrap></Wrap>;
+}
+
+export default Sections;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: orange;
+`;
