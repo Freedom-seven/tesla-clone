68ee18bf37f998c821d94c022d233b6c6727de26
FREEDOM <freewhynane62@gmail.com>
2022-01-26 14:15:38 +0000

    render section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,54 @@ import Sections from "./Sections";
 function Home() {
   return (
     <Container>
-      <Sections />
+      <Sections
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Sections
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Sections
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Sections
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Sections
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarante"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Sections
+        title="Solar fro New Roofs"
+        description="Solar Roof Costs Less Than a New Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Sections
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
     </Container>
   );
 }
